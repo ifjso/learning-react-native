ca39809d4aef40616721aab502e00e8315a806f4
JesusOh <ojsboy@hotmail.com>
2018-10-07 16:36:57 +0900

    Complete Weather

--- chapter-03/Weather/WeatherProject.js
+++ chapter-03/Weather/WeatherProject.js
@@ -1,49 +1,85 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TextInput, ImageBackground } from 'react-native'
 import Forecast from './Forecast'
+import OpenWeatherMap from './open_weather_map'
 
 class WeatherProject extends Component {
     constructor(props) {
         super(props)
-        this.state = { zip: "", forecast: { main: 'AA', description: 'BB', temp: 6} }
+        this.state = { zip: "", forecast: null }
     }
 
     _handleTextChange = event => {
-        this.setState({ zip: event.nativeEvent.text })
+        let zip = event.nativeEvent.text
+        OpenWeatherMap.fetchForecast(zip).then(forecast => {
+            this.setState({ forecast })
+        })
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    You input {this.state.zip}.
-                </Text>
-                { this.state.forecast && <Forecast {...this.state.forecast} /> }
-                <TextInput style={styles.input} onSubmitEditing={this._handleTextChange} />
+                <ImageBackground source={require("./flowers.png")} style={styles.background}>
+                    <View style={styles.overlay}>
+                        <View style={styles.row}>
+                            <Text style={styles.mainText}>
+                                Current weather for
+                            </Text>
+                            <View style={styles.zipContainer}>
+                                <TextInput style={[styles.zipCode, styles.mainText]} 
+                                    onSubmitEditing={this._handleTextChange}
+                                    underlineColorAndroid="transparent"/>
+                            </View>
+                        </View>
+                        { this.state.forecast && <Forecast {...this.state.forecast} /> }
+                    </View>
+                </ImageBackground>
             </View>
         )
     }
 }
 
+const baseFontSize = 16
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#AAAAAA"
+        paddingTop: 30
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: "center",
-        margin: 10
+    background: {
+        width: "100%",
+        height: "100%"
     },
-    input: {
-        fontSize: 20,
-        borderWidth: 2,
-        padding: 2,
-        height: 40,
-        width: 100,
-        textAlign: "center"
+    overlay: {
+        paddingTop: 5,
+        backgroundColor: "#000000",
+        opacity: 0.5,
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    row: {
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        alignItems: "flex-start",
+        padding: 30
+    },
+    zipContainer: {
+        height: baseFontSize + 10,
+        borderBottomColor: "#DDDDDD",
+        borderBottomWidth: 1,
+        marginLeft: 5,
+        marginTop: -1
+    },
+    zipCode: {
+        flex: 1,
+        flexBasis: 1,
+        width: 50,
+        height: baseFontSize
+    },
+    mainText: {
+        fontSize: baseFontSize,
+        color: "#FFFFFF"
     }
 })
 
