d30deaf732554de62b9767dba9e99521227b943a
JesusOh <ojsboy@hotmail.com>
2018-10-09 18:31:26 +0900

    Add SimpleList

--- chapter-04/bestsellers/App.js
+++ chapter-04/bestsellers/App.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import SimpleList from './SimpleList'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <SimpleList />
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
