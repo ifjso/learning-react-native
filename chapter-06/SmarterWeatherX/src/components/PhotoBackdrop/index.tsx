cbdf30130c6f0f0e1f1847ed7c2333d25620fd20
Dval <ojsboy@hotmail.com>
2018-11-26 18:22:53 +0900

    Add PhotoBackdrop component

---
+++ chapter-06/SmarterWeatherX/src/components/PhotoBackdrop/index.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { ImageBackground, CameraRoll } from 'react-native';
+import styles from './style';
+
+type Props = {
+    children?: React.ReactNode
+};
+
+type State = {
+    photoSource: { uri: string }
+};
+
+class PhotoBackdrop extends Component<Props, State> {
+    public state = {
+        photoSource: null
+    };
+
+    public componentDidMount () {
+        CameraRoll
+            .getPhotos({ first: 5 })
+            .then(
+                data => this.setState({ photoSource: { uri: data.edges[1].node.image.uri } }),
+                error => console.warn(error)
+            );
+    }
+
+    public render () {
+        return (
+            <ImageBackground style={styles.backdrop} source={this.state.photoSource} resizeMode="cover" >
+                {this.props.children}
+            </ImageBackground>
+        );
+    }
+}
+
+export default PhotoBackdrop;
---
+++ chapter-06/SmarterWeatherX/src/components/PhotoBackdrop/localImage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ImageBackground } from 'react-native';
+import styles from './style';
+
+type Props = {
+    children?: React.ReactNode
+};
+
+const PhotoBackdrop = (props: Props) => (
+    <ImageBackground style={styles.backdrop} source={require('./flowers.png')} resizeMode="cover">
+        {props.children}
+    </ImageBackground>
+);
+
+export default PhotoBackdrop;
