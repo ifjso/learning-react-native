5562b5ad96e93349182afc782019a49da71eae09
JesusOh <ojsboy@hotmail.com>
2018-12-01 17:04:13 +0900

    Add lodash

--- chapter-07/depends/App.tsx
+++ chapter-07/depends/App.tsx
@@ -1,18 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { random } from 'lodash';
 import Video from 'react-native-video';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    console.log(`Your lucky number is ${random(0, 100)}`);
+
     return (
       <View style={styles.container}>
         <Video
