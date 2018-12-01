14ede53c8970f5411d72ee3d7cdb752ab8395ce0
JesusOh <ojsboy@hotmail.com>
2018-12-01 17:57:44 +0900

    Add native script

---
+++ chapter-07/depends/ios/HelloWorld.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.h
+//  depends
+//
+//  Created by jso on 01/12/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#ifndef HelloWorld_h
+#define HelloWorld_h
+
+
+#endif /* HelloWorld_h */
---
+++ chapter-07/depends/ios/HelloWorld.m
@@ -0,0 +1,9 @@
+//
+//  HelloWorld.m
+//  depends
+//
+//  Created by jso on 01/12/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
