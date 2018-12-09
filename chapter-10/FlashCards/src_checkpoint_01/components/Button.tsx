2b188e65055fa45188ab8e1ff1b7f9c3715e82c0
JesusOh <ojsboy@hotmail.com>
2018-12-09 19:05:44 +0900

    Add Button component

---
+++ chapter-10/FlashCards/src_checkpoint_01/components/Button.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import colors from '../styles/colors';
+
+const Button = ({
+  disabled = false,
+  onPress,
+  style,
+  children
+}: {
+  disabled: boolean;
+  onPress: () => void;
+  style?: object;
+  children?: React.ReactNode;
+}) => {
+  const opacity: number = disabled ? 1 : 0.5;
+
+  return (
+    <TouchableOpacity
+      activeOpacity={opacity}
+      onPress={onPress}
+      style={[styles.wideButton, style]}
+    >
+      {children}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  wideButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    margin: 10,
+    backgroundColor: colors.pink
+  }
+});
+
+export default Button;
