2bb4917f28fc1073cd928f846c9fa6cddb20e9e9
JesusOh <ojsboy@hotmail.com>
2018-12-09 18:40:57 +0900

    Add Header component

--- chapter-10/FlashCards/src_checkpoint_01/components/HeadingText.tsx
+++ chapter-10/FlashCards/src_checkpoint_01/components/HeadingText.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, Dimensions } from 'react-native';
 import { fonts, scalingFactors } from '../styles/fonts';
 
-const { width } = Dimensions.get('window');
-
 type Props = {
-  style: object;
+  style?: object;
   children: React.ReactNode;
 };
 
 const HeadingText = (props: Props) => (
   <Text style={[props.style, fonts.big, scaled.big]}>{props.children}</Text>
 );
+
+const { width } = Dimensions.get('window');
 
 const scaled = StyleSheet.create({
   big: { fontSize: width / scalingFactors.big }
