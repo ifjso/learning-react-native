446b5cd1a2ca54e1a71e803d6f1144200c491d2a
JesusOh <ojsboy@hotmail.com>
2018-12-14 18:16:33 +0900

    Remove image import

--- chapter-10/FlashCards/src_checkpoint_02/components/Header/index.tsx
+++ chapter-10/FlashCards/src_checkpoint_02/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
 import styles from './styles';
 import HeadingText from '../HeadingText';
 import Logo from './Logo';
