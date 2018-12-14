111f5df6280ceb379584203d4459a89b313c88f9
JesusOh <ojsboy@hotmail.com>
2018-12-14 18:15:55 +0900

    Config stack navigator

--- chapter-10/FlashCards/src_checkpoint_02/components/FlashCards.tsx
+++ chapter-10/FlashCards/src_checkpoint_02/components/FlashCards.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Heading from './Header';
+import { createStackNavigator } from 'react-navigation';
+import Logo from './Header/logo';
 import DeckScreen from './DeckScreen';
 import CardScreen from './CardScreen';
 import ReviewScreen from './ReviewScreen';
 
-const FlashCards = () => {
-  const _renderScene = () => <DeckScreen />;
-
-  return (
-    <View style={styles.container}>
-      <Heading />
-      {_renderScene()}
-    </View>
-  );
+const headerOptions = {
+  headerStyle: { backgroundColor: '#FFFFFF' },
+  headerLeft: <Logo />
 };
 
-const styles = StyleSheet.create({
-  container: { flex: 1, marginTop: 30 }
+const navigator = createStackNavigator({
+  Home: { screen: DeckScreen, navigationOptions: headerOptions },
+  Review: { screen: ReviewScreen, navigationOptions: headerOptions },
+  CardCreation: { screen: CardScreen, navigationOptions: headerOptions }
 });
 
-export default FlashCards;
+export default navigator;
