b7621c87c035938b5526830a00cdb9a01a3800d5
JesusOh <ojsboy@hotmail.com>
2018-12-22 13:16:37 +0900

    Add Input component

---
+++ chapter-10/FlashCards/src_checkpoint_03/components/Input.tsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+import colors from '../styles/colors';
+import { fonts } from '../styles/fonts';
+
+type Props = {
+  style?: object;
+  clearOnSubmit: boolean;
+  onEntry: (text: string) => void;
+  onChange: (text: string) => void;
+};
+
+type State = {
+  text: string;
+};
+
+class Input extends Component<Props, State> {
+  static defaultProps = {
+    clearOnSubmit: true
+  };
+
+  constructor(props: Props) {
+    super(props);
+    alert(props.clearOnSubmit);
+    this.state = { text: '' };
+  }
+
+  _create = () => {
+    this.props.onEntry(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  _onSubmit = (ev: any) => {
+    this.props.onEntry(ev.nativeEvent.text);
+    if (this.props.clearOnSubmit) {
+      this.setState({ text: '' });
+    }
+  };
+
+  _onChange = (text: string) => {
+    this.setState({ text });
+    if (this.props.onChange) {
+      this.props.onChange(text);
+    }
+  };
+
+  render() {
+    return (
+      <TextInput
+        style={[
+          styles.nameField,
+          styles.wideButton,
+          fonts.normal,
+          this.props.style
+        ]}
+        multiline={false}
+        autoCorrect={false}
+        onChangeText={this._onChange}
+        onSubmitEditing={this._onSubmit}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  nameField: { backgroundColor: colors.tan, height: 60 },
+  wideButton: { justifyContent: 'center', padding: 10, margin: 10 }
+});
+
+export default Input;
