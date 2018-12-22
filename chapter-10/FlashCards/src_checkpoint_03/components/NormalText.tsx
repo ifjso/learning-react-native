ab74a86dff23bbb1cca42add9e07ec0ac3ebc502
JesusOh <ojsboy@hotmail.com>
2018-12-22 12:42:54 +0900

    Add NormalText component

---
+++ chapter-10/FlashCards/src_checkpoint_03/components/NormalText.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, Dimensions } from 'react-native';
+import { fonts, scalingFactors } from '../styles/fonts';
+
+const NormalText = ({
+  style,
+  children
+}: {
+  style?: object;
+  children: React.ReactNode;
+}) => <Text style={[style, fonts.normal, scaled.normal]}>{children}</Text>;
+
+const { width } = Dimensions.get('window');
+
+const scaled = StyleSheet.create({
+  normal: { fontSize: width / scalingFactors.normal }
+});
+
+export default NormalText;
