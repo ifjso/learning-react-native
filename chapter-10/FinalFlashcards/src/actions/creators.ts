84cbab5c94265e020515c823e460a420d5f94f8f
JesusOh <ojsboy@hotmail.com>
2018-12-26 00:19:44 +0900

    Add actions

---
+++ chapter-10/FinalFlashcards/src/actions/creators.ts
@@ -0,0 +1,32 @@
+import {
+  ADD_DECK,
+  ADD_CARD,
+  REVIEW_DECK,
+  STOP_REVIEW,
+  NEXT_REVIEW,
+  LOAD_DATA
+} from './types';
+
+import Card from '../data/Card';
+import Deck from '../data/Deck';
+
+export const addDeck = (name: string) => ({
+  type: ADD_DECK,
+  data: new Deck(name)
+});
+
+export const addCard = (front: string, back: string, deckID: string) => ({
+  type: ADD_CARD,
+  data: new Card(front, back, deckID)
+});
+
+export const reviewDeck = (deckID: string) => ({
+  type: REVIEW_DECK,
+  data: { deckID }
+});
+
+export const stopReview = () => ({ type: STOP_REVIEW, data: {} });
+
+export const nextReview = () => ({ type: NEXT_REVIEW, data: {} });
+
+export const loadData = (data: any) => ({ type: LOAD_DATA, data });
