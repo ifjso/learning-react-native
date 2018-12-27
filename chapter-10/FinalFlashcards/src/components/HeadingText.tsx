20c9b666044aa5b5b96ad1ca610f341247adbc32
Dval <ojsboy@hotmail.com>
2018-12-27 17:14:45 +0900

    Add HeadingText component

---
+++ chapter-10/FinalFlashcards/src/components/HeadingText.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { StyleSheet, Text, Dimensions } from 'react-native';
+import { fonts, scalingFactors } from '../styles/fonts';
+
+type Props = {
+  style: object;
+};
+
+const HeadingText: FC<Props> = ({ style, children }) => (
+  <Text style={[style, fonts.big, scaled.big]}>{children}</Text>
+);
+
+HeadingText.displayName = 'HeadingText';
+
+const { width } = Dimensions.get('window');
+
+const scaled = StyleSheet.create({
+  big: { fontSize: width / scalingFactors.big }
+});
+
+export default HeadingText;
