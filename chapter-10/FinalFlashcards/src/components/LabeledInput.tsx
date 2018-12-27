368afce3789c1c27b21663d4ef5fc370ecd2c951
Dval <ojsboy@hotmail.com>
2018-12-27 17:07:10 +0900

    Add LabeledInput component

---
+++ chapter-10/FinalFlashcards/src/components/LabeledInput.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Input from './Input';
+import NormalText from './NormalText';
+
+type Props = {
+  label: string;
+  clearOnSubmit: boolean;
+  inputStyle: object;
+  onEntry: () => void;
+  onChange: () => void;
+};
+
+const LabeledInput: FC<Props> = ({
+  label,
+  clearOnSubmit,
+  inputStyle,
+  onEntry,
+  onChange
+}) => (
+  <View style={styles.wrapper}>
+    <NormalText style={styles.label}>{label}</NormalText>
+    <Input
+      onEntry={onEntry}
+      onChange={onChange}
+      clearOnSubmit={clearOnSubmit}
+      style={inputStyle}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  label: { paddingLeft: 10 },
+  wrapper: { padding: 5 }
+});
+
+export default LabeledInput;
